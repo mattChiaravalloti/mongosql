a1c2488f905ee3089d6acc2d5bd80c33b432c46b
Matthew Chiaravalloti <matthew.chiaravalloti@mongodb.com>
2026-07-27 14:40:44 -0400

    SQL-3293: Fix map expr algebrizer tests

--- mongosql/src/algebrizer/test/expressions/higher_order_functions.rs
+++ mongosql/src/algebrizer/test/expressions/higher_order_functions.rs
@@ -3,6 +3,7 @@ use super::*;
 mod map {
     use super::*;
 
+    // MAP([1], 1)
     test_algebrize!(
         no_variables,
         method = algebrize_expression,
@@ -27,6 +28,7 @@ mod map {
             })),
     );
 
+    // MAP(foo.a, 1)
     test_algebrize!(
         nullable,
         method = algebrize_expression,
@@ -46,9 +48,10 @@ mod map {
         )),
         input =
             ast::Expression::HigherOrderFunction(ast::HigherOrderFunctionExpr::Map(ast::MapExpr {
-                array: Box::new(ast::Expression::Array(vec![ast::Expression::Literal(
-                    ast::Literal::Integer(1)
-                )])),
+                array: Box::new(ast::Expression::Subpath(ast::SubpathExpr {
+                    expr: Box::new(ast::Expression::Identifier("foo".to_string())),
+                    subpath: "a".to_string(),
+                })),
                 f: Box::new(ast::FunctionArgument::Expr(ast::Expression::Literal(
                     ast::Literal::Integer(1)
                 ))),
@@ -65,6 +68,7 @@ mod map {
         },
     );
 
+    // MAP([1], this)
     test_algebrize!(
         with_this_variable,
         method = algebrize_expression,
@@ -92,6 +96,7 @@ mod map {
             })),
     );
 
+    // MAP([1, NULL], this)
     test_algebrize!(
         with_nullable_this_variable,
         method = algebrize_expression,
@@ -99,7 +104,10 @@ mod map {
         expected = Ok(mir::Expression::HigherOrderFunction(
             mir::HigherOrderFunctionApplication::Map(mir::MapExpr {
                 array: Box::new(mir::Expression::Array(mir::ArrayExpr {
-                    array: vec![mir::Expression::Literal(mir::LiteralValue::Null)]
+                    array: vec![
+                        mir::Expression::Literal(mir::LiteralValue::Integer(1)),
+                        mir::Expression::Literal(mir::LiteralValue::Null),
+                    ]
                 })),
                 f: Box::new(mir::Expression::Variable(mir::Variable {
                     name: "this".to_string(),
@@ -110,15 +118,17 @@ mod map {
         )),
         input =
             ast::Expression::HigherOrderFunction(ast::HigherOrderFunctionExpr::Map(ast::MapExpr {
-                array: Box::new(ast::Expression::Array(vec![ast::Expression::Literal(
-                    ast::Literal::Integer(1)
-                )])),
+                array: Box::new(ast::Expression::Array(vec![
+                    ast::Expression::Literal(ast::Literal::Integer(1)),
+                    ast::Expression::Literal(ast::Literal::Null),
+                ])),
                 f: Box::new(ast::FunctionArgument::Expr(ast::Expression::Identifier(
                     "this".to_string()
                 ))),
             })),
     );
 
+    // MAP([1], this + this)
     test_algebrize!(
         with_this_variable_multiple_times,
         method = algebrize_expression,
@@ -162,18 +172,15 @@ mod map {
             })),
     );
 
+    // MAP([1], this + foo.this)
     test_algebrize!(
         with_this_variable_and_field_access,
         method = algebrize_expression,
         in_implicit_type_conversion_context = false,
         expected = Ok(mir::Expression::HigherOrderFunction(
             mir::HigherOrderFunctionApplication::Map(mir::MapExpr {
-                array: Box::new(mir::Expression::FieldAccess(mir::FieldAccess {
-                    expr: Box::new(mir::Expression::Reference(mir::ReferenceExpr {
-                        key: ("foo", 0u16).into(),
-                    })),
-                    field: "a".to_string(),
-                    is_nullable: true,
+                array: Box::new(mir::Expression::Array(mir::ArrayExpr {
+                    array: vec![mir::Expression::Literal(mir::LiteralValue::Integer(1))]
                 })),
                 f: Box::new(mir::Expression::ScalarFunction(
                     mir::ScalarFunctionApplication {
@@ -194,7 +201,7 @@ mod map {
                         is_nullable: false,
                     }
                 )),
-                is_nullable: true,
+                is_nullable: false,
             })
         )),
         input =
